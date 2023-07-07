f0c4fe46867c07c7b7e0d95b6e35e4f9e6627c63
Antoine Aube <aaube3@gmail.com>
2023-07-06 21:15:58 -0600

    add small fix to node spec

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,10 +2,11 @@ require 'pry'
 require 'node.rb'
 
 RSpec.describe Node do 
-  describe '#intialize'
+  describe '#intialize' do
 
     it "is an instance of node" do 
       node_1 = Node.new
       expect(node_1).to be_a(Node)
     end
+  end 
 end
