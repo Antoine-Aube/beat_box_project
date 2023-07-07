858cc79c1b88161edbc571d605bcb10448421125
Antoine Aube <aaube3@gmail.com>
2023-07-06 22:06:11 -0600

    added test to check data instance varibale on node object

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -5,8 +5,15 @@ RSpec.describe Node do
   describe '#intialize' do
 
     it "is an instance of node" do 
-      node_1 = Node.new
+      node_1 = Node.new("bleep")
       expect(node_1).to be_a(Node)
     end
-  end 
+  end
+end
+ RSpec.describe Node do
+    it "has a data" do
+      node_1 = Node.new("plop")
+
+      expect(node_1.data).to eq("plop")    
+    end
 end
